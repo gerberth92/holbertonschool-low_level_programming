5f48523dec093ff9ee683be8470e5627fbbd42af
gerberth92 <gerberthcontreras@gmail.com>
2023-10-18 11:06:44 -0700

    numeros

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - punto de entrada
+ * Return: 0
+ */
+
+int main(void)
+{
+	int decimal = 0;
+
+	while (decimal < 10)
+	{
+		printf("%d", decimal);
+		decimal++;
+	}
+	putchar('\n');
+	return (0);
+}
