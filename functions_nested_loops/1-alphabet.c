ad1cf31ccccd31ce9e678535285538a71d3fd5fe
gerberth92 <gerberthcontreras@gmail.com>
2023-10-18 14:16:40 -0700

    abc

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - void
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char abc;
+
+	for (abc = 'a'; abc <= 'z'; abc++)
+	{
+		_putchar(abc);
+	}
+	_putchar('\n');
+}
