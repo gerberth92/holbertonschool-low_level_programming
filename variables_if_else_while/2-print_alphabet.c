9b94a65036c8e22331b9572c8a02aa4c1db85f76
gerberth92 <gerberthcontreras@gmail.com>
2023-10-17 19:28:01 -0700

    putchar

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - punto de entrada
+ * Return: 0
+ */
+
+int main(void)
+{
+	char abc = 'a';
+
+	while (abc <= 'z')
+	{
+		putchar(abc);
+		abc++;
+	}
+	return (0);
+}
