3ac9e9cb9ecfd2543d090608022046d0410f84b4
gerberth92 <gerberthcontreras@gmail.com>
2023-10-21 11:11:13 -0700

    tabla del 9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * times_table - punto de entrada
+ * Return: dato
+ */
+void times_table(void)
+{
+	int i;
+	int j;
+	int producto;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			producto = i * j;
+
+			if (producto >= 10)
+			{
+				_putchar(producto / 10 + '0');
+			}
+			_putchar(producto % 10 + '0');
+			
+			if (j < 9)
+			{
+				_putchar(',');
+
+				if (i * (j + 1 ) < 10)
+				{
+					_putchar(' ');
+				}
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
