e39aa15ba69b425847cf531d634e3eca3f0aa594
gerberth92 <gerberthcontreras@gmail.com>
2023-10-23 20:26:34 -0700

    lent

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - punto de entrada
+ * @s: caracter
+ * Return: tamaño del string
+ */
+int _strlen(char *s)
+{
+	int lent;
+
+	for (lent = 0; s[lent] != '\0'; lent++)
+	{
+	}
+	return (lent);
+}
