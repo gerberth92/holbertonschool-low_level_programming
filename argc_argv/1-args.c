971b5c151099679aab5462358ca4c4cd516951ab
gerberth92 <gerberthcontreras@gmail.com>
2023-10-25 14:38:24 -0700

    print number of argument

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - punto de entrada
+ * @argc: contador
+ * @argv: array
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc - 1);
+	return (0);
+}
