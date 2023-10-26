727b72bafd9b33a0442cf9da87831679337905ed
gerberth92 <gerberthcontreras@gmail.com>
2023-10-25 20:13:16 -0700

    print multiplicacion con argc y argv

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - punto de entrada
+ * @argc: contador
+ * @argv: puntero a un array
+ * Return: 1 si es error
+ */
+int main(int argc, char *argv[])
+{
+	int result;
+	int argc1, argc2;
+
+	if (argc == 3)
+	{
+		argc1 = atoi(argv[1]);
+		argc2 = atoi(argv[2]);
+
+		result = argc1 * argc2;
+
+		printf("%d\n", result);
+	}
+	else
+	{
+		printf("Error\n");
+	}
+	return (1);
+}
