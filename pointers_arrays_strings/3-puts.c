2a33075d765eb9400691338e72de06293074227c
gerberth92 <gerberthcontreras@gmail.com>
2023-10-27 09:29:32 -0700

    print with _putchar

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - punto de entrada
+ * @str: cadena
+ */
+void _puts(char *str)
+{
+	int c;
+
+	for (c = 0; str[c] != '\0'; c++)
+	{
+		_putchar((str[c]));
+	}
+	_putchar('\n');
+}
