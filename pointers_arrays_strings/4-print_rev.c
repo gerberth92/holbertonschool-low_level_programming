4b0fc72f7eb79b8b19eb9c25479f15c25c31c59e
gerberth92 <gerberthcontreras@gmail.com>
2023-10-27 19:44:25 -0700

    print reverse

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_rev - putno de entrada
+ * @str: dato
+ */
+void print_rev(char *s)
+{
+	int c;
+	int b;
+
+	for (c = 0; s[c] != '\0'; c++)
+	{
+	}
+	for (b = c - 1; b >= 0; b--)
+	{
+		_putchar(s[b]);
+	}
+	_putchar('\n');
+}
