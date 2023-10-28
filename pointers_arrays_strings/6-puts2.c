9eed8c9c82dcc56db1788f52c5d2574805a3765e
gerberth92 <gerberthcontreras@gmail.com>
2023-10-28 13:28:09 -0700

    mejorando el codigo para que termine hantes de nulo

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -7,10 +7,14 @@
 void puts2(char *str)
 {
 	int c;
+	int b;
 
-	for (c = 0; str[c] != '\0'; c += 2)
+	for (c = 0; str[c] != '\0'; c++)
 	{
-		_putchar(str[c]);
+	}
+	for  (b = 0; b < c; b += 2)
+	{
+		_putchar(str[b]);
 	}
 	_putchar('\n');
 }
