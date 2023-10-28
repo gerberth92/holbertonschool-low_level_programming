fa0eb2489d3ae4609b32c469924f1f0a0ca25b7f
gerberth92 <gerberthcontreras@gmail.com>
2023-10-28 14:40:48 -0700

    imprimiendo numeros de la mitad en adelante si son pares o impares

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * puts_half - imprime la mitad del string
+ * @str: puntero
+ */
+void puts_half(char *str)
+{
+	int c;
+	int b;
+
+	for (c = 0; str[c] != '\0'; c++)
+	{
+	}
+	if (c % 2 == 0)
+	{
+		for (b = c / 2; b < c; b++)
+		{
+			_putchar(str[b]);
+		}
+	}
+	else
+	{
+		for (b = (c - 1) / 2; b < c; b++)
+		{
+			_putchar(str[b]);
+		}
+	}
+	_putchar('\n');
+}
