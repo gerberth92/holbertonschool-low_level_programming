4cbfce61c1b65b92ced8d5d162821e45a898f1a9
gerberth92 <gerberthcontreras@gmail.com>
2023-10-29 10:25:51 -0700

    conteo de caracteres

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - punto de entrada
+ * @s: dato
+ * Return: lent
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return 1 + _strlen_recursion(s + 1);
+	}
+}
