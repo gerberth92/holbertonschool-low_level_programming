d177ebe5ff218d536182f76e0023d2d488961c55
gerberth92 <gerberthcontreras@gmail.com>
2023-10-31 09:17:15 -0700

    declarando struc

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _dog_h_
+#define _dog_h_
+
+/**
+ * struct dog - datos de perros
+ * @name: nombre del perro
+ * @age: edad del perro
+ * @owner :dueño del perro
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
