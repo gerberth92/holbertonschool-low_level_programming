eae56c4e02d43771c8f49937be7bf4cd5f8d3fca
gerberth92 <gerberthcontreras@gmail.com>
2023-11-03 15:12:00 -0700

    creando una copia de un puntero

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,27 +1,31 @@
 #include "main.h"
 
 /**
- * _strdup - funcion que retorna un punto a una nueva locacion
- * 
- *
- *
+ * _strdup - funcion que retorna un puntero a una nueva locacion
+ * @str: puntero solo lectura
+ * Return: array que es la copia de str
  */
 char *_strdup(char *str)
 {
+	char *array;
+	int c;
+	int i;
+
 	if (str == NULL)
 	{
 		return (NULL);
 	}
 
-	char *array;
-	int c;
-	int i;
-
-	for (c = 0; str[c] <= '\0'; c++)
+	for (c = 0; str[c] != '\0'; c++)
 	{
 	}
 
 	array = malloc(sizeof(char) * c);
+
+	if (array == NULL)
+	{
+		return (NULL);
+	}
 
 	for (i = 0; i <= c; i++)
 	{
--- malloc_free/main.h
+++ malloc_free/main.h
@@ -2,8 +2,10 @@
 #define _main_h_
 
 #include <stdlib.h>
+#include <stdio.h>
 
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
 
 #endif
