fad21fc9ed0af0d5a879e89262cfb6c176eff246
gerberth92 <gerberthcontreras@gmail.com>
2023-11-04 13:57:47 -0700

    imprimiendo una estrcutura

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+
+/**
+ * print_dog - imprime una estructura
+ * @d: estructura a imprimir
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+
+	(d->name == NULL) ? printf("Name: (nil)\n") : printf("Name: %s\n", d->name);
+
+	printf("Name: %f\n", d->age);
+
+	(d->owner == NULL) ? printf("owner: (nil)\n") :
+	printf("Name: %s\n", d->owner);
+}
