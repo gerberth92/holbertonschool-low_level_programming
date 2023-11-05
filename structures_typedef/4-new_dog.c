7f6f8f902323c18b7acd5ebc5f6dae68827f8d96
gerberth92 <gerberthcontreras@gmail.com>
2023-11-04 18:34:55 -0700

    mejorando el codigo declarando mas funciones

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -3,6 +3,7 @@
 /**
  * contador - cuanta los elementos de una array
  * @dato: array
+ * Return: el largo de la array
  */
 int contador(char *dato)
 {
@@ -11,6 +12,25 @@ int contador(char *dato)
 	for (i = 0; dato[i] != '\0'; i++)
 	{}
 	return (i);
+}
+
+/**
+ * copia - copia una array
+ * @dato: dato a copiar
+ * @copia: copia
+ * Return: copia
+ */
+char *copia(char *dato, char *copia)
+{
+	int i;
+
+	for (i = 0; dato[i] != '\0'; i++)
+	{
+		copia[i] = dato[i];
+	}
+	copia[i] = '\0';
+
+	return (copia);
 }
 
 /**
@@ -23,42 +43,35 @@ int contador(char *dato)
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
-	char *copy_name, *copy_owner;
-	int c;
+	char *copy_name;
+	char *copy_owner;
 
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 	{
 		return (NULL);
 	}
-
 	copy_name = malloc(contador(name) + 1);
 
 	if (copy_name == NULL)
 	{
+		free(new_dog);
 		return (NULL);
 	}
-	for (c = 0; name[c] != '\0'; c++)
-	{
-		copy_name[c] = name[c];
-	}
-	copy_name[c] = '\0';
 
-	new_dog->name = copy_name;
+	new_dog->name = copia(name, copy_name);
 	new_dog->age = age;
 
 	copy_owner = malloc(contador(owner) + 1);
 
 	if (copy_owner == NULL)
 	{
+		free(copy_name);
+		free(new_dog);
 		return (NULL);
 	}
-	for (c = 0; owner[c] != '\0'; c++)
-	{
-		copy_owner[c] = owner[c];
-	}
-	copy_owner[c] = '\0';
 
-	new_dog->owner = copy_owner;
+	new_dog->owner = copia(owner, copy_owner);
+
 	return (new_dog);
 }
